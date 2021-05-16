af8efade0a3121af042910d4764cc16d7d26ab1c
Brendan Bergen <brendandbergen@gmail.com>
2021-05-16 10:23:47 -0500

    Adding iteration

---
+++ iteration/repeat.go
@@ -0,0 +1,8 @@
+package iteration
+
+import "strings"
+
+// Return a string str repeated count times
+func Repeat(str string, count int) string {
+	return strings.Repeat(str, count)
+}
---
+++ iteration/repeat_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a", 5)
+	want := "aaaaa"
+	if got != want {
+		t.Errorf("wanted %q but got %q", want, got)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("ha", 3)
+	fmt.Println(repeat)
+	// Output: hahaha
+}
